d4fd490ce16802dfd98535079d2414d1c998f8dd
paulg44 <paulgarton1984@hotmail.com>
2023-07-06 20:28:07 +0100

    create raect app & add button compoenent

---
+++ dictionary-app/src/Components/Button/Button.js
@@ -0,0 +1,5 @@
+// Component for Button
+
+function Button(props) {
+  return <button className="standardBtn">{props.btnText}</button>;
+}
--- tests/dictionary.test.js
+++ tests/dictionary.test.js
@@ -1,34 +1,34 @@
 // @ts-check
-const { test, expect } = require('@playwright/test');
+const { test, expect } = require("@playwright/test");
 
 // Global Variables
-const homePage = 'http://127.0.0.1:5501/';
+const homePage = "http://127.0.0.1:5501/";
 
 // Title Test
-test('page title', async ({ page }) => {
-  const pageTitle = 'Dictionary App';
+test("page title", async ({ page }) => {
+  const pageTitle = "Dictionary App";
   await page.goto(homePage);
 
   await expect(page).toHaveTitle(pageTitle);
 });
 
 // Input Test
-test('test input is correct value', async ({ page }) => {
-  const input = page.getByLabel('Enter your word');
+test("test input is correct value", async ({ page }) => {
+  const input = page.getByLabel("Enter your word");
   await page.goto(homePage);
-  await input.fill('word');
+  await input.fill("word");
 
-  await expect(input).toHaveValue('word');
+  await expect(input).toHaveValue("word");
 });
 
 // Check that noun shows when button clicked
-test('define adds a noun', async ({ page }) => {
-  const input = page.getByLabel('Enter your word');
-  const defineBtn = page.getByRole('button', { name: 'Define' });
-  const nounDisplay = page.getByTestId('noun');
+test("define adds a noun", async ({ page }) => {
+  const input = page.getByLabel("Enter your word");
+  const defineBtn = page.getByRole("button", { name: "Define" });
+  const nounDisplay = page.getByTestId("noun");
 
   await page.goto(homePage);
-  await input.fill('word');
+  await input.fill("word");
   await defineBtn.click();
   await page.waitForTimeout(3000);
 
@@ -36,13 +36,13 @@ test('define adds a noun', async ({ page }) => {
 });
 
 // Check that refresh button clears input
-test('refresh clears input', async ({ page }) => {
-  const input = page.getByLabel('Enter your word');
-  const defineBtn = page.getByRole('button', { name: 'Define' });
-  const refreshBtn = page.getByRole('button', { name: 'Refresh' });
+test("refresh clears input", async ({ page }) => {
+  const input = page.getByLabel("Enter your word");
+  const defineBtn = page.getByRole("button", { name: "Define" });
+  const refreshBtn = page.getByRole("button", { name: "Refresh" });
 
   await page.goto(homePage);
-  await input.fill('word');
+  await input.fill("word");
   await defineBtn.click();
   await page.waitForTimeout(3000);
   await refreshBtn.click();
