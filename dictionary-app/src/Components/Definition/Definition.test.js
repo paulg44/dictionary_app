76f1cc6e85f53a6b0411fdd5f3dfd292dab9500e
paulg44 <paulgarton1984@hotmail.com>
2023-07-16 14:52:21 +0100

    a few more basic tests and a mock api call test

--- dictionary-app/src/Components/Definition/Definition.test.js
+++ dictionary-app/src/Components/Definition/Definition.test.js
@@ -1,5 +1,6 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, act, fireEvent } from "@testing-library/react";
 import DefinitionContainer from "./Definition";
+import App from "../../App";
 
 /* Tests 
      - Test noun renders when api called
@@ -12,4 +13,30 @@ test("renders page", () => {
   render(<DefinitionContainer />);
   const linkElement = screen.getByText(/Noun/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+// Is noun displayed after API called
+test("noun displayed", async () => {
+  //   Mock the fetch function
+  jest.spyOn(window, "fetch").mockResolvedValue({
+    ok: true,
+    json: jest.fn().mockResolvedValue({}), // Mock response is empty object
+  });
+
+  render(<App />);
+
+  const input = screen.getByRole("textbox");
+  const defineBtn = screen.getByRole("button", { name: "Define" });
+  expect(input && defineBtn).toBeInTheDocument();
+
+  //   Simulate user input
+  act(() => {
+    fireEvent.change(input, { target: { value: "word" } });
+    fireEvent.click(defineBtn);
+  });
+
+  const nounDef = screen.getByRole("heading", { name: "Noun:" });
+  expect(nounDef).toBeInTheDocument();
+
+  window.fetch.mockRestore();
 });
--- dictionary-app/src/Components/EnterWord/EnterWord.test.js
+++ dictionary-app/src/Components/EnterWord/EnterWord.test.js
@@ -1,5 +1,13 @@
-import { render, screen } from "@testing-library/react";
+import {
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+  act,
+} from "@testing-library/react";
 import EnterWordContainer from "./EnterWord";
+import App from "../../App";
+import { expect } from "@playwright/test";
 
 /* Tests 
      - Test a word can be entered into user input
@@ -15,8 +23,65 @@ test("renders page", () => {
 });
 
 // Enter Word
-test("can a word be entered to input", () => {
-  render(<EnterWordContainer />);
+test("can a word be entered to input", async () => {
+  const simulatedWord = "word";
+
+  render(<EnterWordContainer userInput={simulatedWord} />);
+
   const input = screen.getByRole("textbox");
   expect(input).toBeInTheDocument();
+
+  act(() => {
+    fireEvent.change(input, { target: { value: simulatedWord } });
+  });
+  expect(input.value).toBe(simulatedWord);
 });
+
+// Test API is called when define btn clicked
+test("api called when define btn clicked", async () => {
+  //   Mock the fetch function
+  jest.spyOn(window, "fetch").mockResolvedValue({
+    ok: true,
+    json: jest.fn().mockResolvedValue({}), // Mock response is empty object
+  });
+
+  render(<App />);
+
+  const input = screen.getByRole("textbox");
+  const defineBtn = screen.getByRole("button", { name: "Define" });
+  expect(input && defineBtn).toBeInTheDocument();
+
+  //   Simulate user input
+  act(() => {
+    fireEvent.change(input, { target: { value: "word" } });
+    fireEvent.click(defineBtn);
+  });
+
+  //   Wait for API call to complete
+  await act(async () => {
+    await waitFor(() => {
+      expect(window.fetch).toHaveBeenCalledTimes(2);
+    });
+  });
+
+  window.fetch.mockRestore();
+});
+
+// // If word does not exist render alert box
+// test("alert box renders", () => {
+//   const simulatedWord = "jhfj";
+
+//   render(<App userInput={simulatedWord} />);
+
+//   const input = screen.getByRole("textbox");
+//   const defineBtn = screen.getByRole("button", { name: "Define" });
+//   expect(input && defineBtn).toBeInTheDocument();
+
+//   fireEvent.change(input, { target: { value: simulatedWord } });
+//   fireEvent.click(defineBtn);
+
+//   const alertBox = screen.getByText(
+//     "Word not found, or incorrectly spelt, please try again."
+//   );
+//   expect(alertBox).toBeInTheDocument();
+// });
